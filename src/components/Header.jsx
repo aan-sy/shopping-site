cb8cabef9c73655c8065b5ef5c3022187fe73441
soyeon <soylee57@naver.com>
2023-12-26 13:15:38 +0900

    Header: UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      헤더
+    <header className='flex gap-x-4 text-base justify-between items-center mb-8'>
+      <Link to='/'>
+        <h1 className='w-60'><img src='/images/logo.png' alt='urbanic30' /></h1>
+      </Link>
+      <div className='flex gap-x-4 justify-end w-full md:w-28'>
+        <Link to='/order/cart'>CART</Link>
+        <Link to=''>LOGIN</Link>
+      </div>
+      <Link to='/shop' className='md:order-first md:w-28'>SHOP</Link>
     </header>
   );
 }
