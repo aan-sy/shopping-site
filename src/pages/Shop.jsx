0bbdea89fa030ae7af2ffd4e61ce01c4e154e967
Soyeon <soylee57@naver.com>
2024-01-03 15:18:39 +0900

    Shop: UI 수정 (처음 렌더링 되면 ALL 메뉴 선택)

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { getProducts } from '../api/firebase';
@@ -8,18 +8,23 @@ const linkStyle = 'w-40 hover:text-black focus:text-black';
 
 export default function Shop() {
   const { category } = useParams();
+  const [isAll, setIsAll] = useState(true);
   const {isLoading, error, data: products} = useQuery({ 
     queryKey: ['products'], 
     queryFn: async () => getProducts(),
     staleTime: 1000 * 60 * 5,
   })
+  function handleClick(e) {
+    if (e.target.localName !== 'a') return;
+    e.target.innerText === 'ALL' ? setIsAll(true) : setIsAll(false);
+  }
 
   if (isLoading) {return <p>Loading...</p>}
   if (error) {console.log(console.error(error))}
   return (
     <section >
-      <nav className='flex flex-col gap-1 text-lg mb-4 text-gray-500'>
-        <Link to='/shop' className={linkStyle}>All</Link>
+      <nav className='flex flex-col gap-1 text-lg mb-4 text-gray-400' onClick={handleClick}>
+        <Link to='/shop' className={`${linkStyle} ${isAll ? 'text-black' : 'text-gray-400'}`}>All</Link>
         <Link to='/shop/outer'className={linkStyle}>Outer</Link>
         <Link to='/shop/top' className={linkStyle}>Top</Link>
         <Link to='/shop/bottom' className={linkStyle}>Bottom</Link>
